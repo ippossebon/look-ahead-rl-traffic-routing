6bdd776d6f9a38a43e6c97ff6ed67b1514c4c4f5
Isadora <isadorapossebon@gmail.com>
2019-04-09 07:50:26 -0300

    Add example data

--- flow-size-prediction/main.py
+++ flow-size-prediction/main.py
@@ -1,3 +1,11 @@
+from math import sqrt
+from random import shuffle
+from sklearn import svm, tree
+from sklearn.ensemble import VotingClassifier, AdaBoostClassifier, BaggingClassifier
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import KFold
+from sklearn.neural_network import MLPClassifier
 
 class FlowSizePredictor(object):
     # Aprendizado supervisionado
@@ -5,5 +13,21 @@ class FlowSizePredictor(object):
     def __init__(self):
         pass
 
-    def neuralNetwork(self):
-        pass
+    def adaBoostMLP(self):
+        classifier_mlp = MLPClassifier(
+            solver='lbfgs',
+            alpha=1e-5,
+            hidden_layer_sizes=(60, 40), # configuração usada por Poupart2016: input de 106; saída de 1 neurônio, indicando se é mice ou elephant flow
+            random_state=1
+        )
+
+        meta_learner = AdaBoostClassifier(
+        base_estimator=classifier_dt,
+        n_estimators=15,
+        algorithm='SAMME'
+        )
+    meta_learner = meta_learner.fit(training_set, training_set_classification)
+
+    result = meta_learner.predict(evaluation_set)
+    predictions = []
+    predictions = list(result.tolist())
