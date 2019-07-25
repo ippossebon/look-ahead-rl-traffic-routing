5a1db07c0342fb743cfd33f387cdb0ad951ead3a
Isadora <isadorapossebon@gmail.com>
2019-07-25 10:08:24 -0300

    fix monitor thread

--- multipath-controller.py
+++ multipath-controller.py
@@ -1,7 +1,7 @@
 from ryu.base import app_manager
 from ryu.controller import mac_to_port
 from ryu.controller import ofp_event
-from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER, DEAD_DISPATCHER
 from ryu.controller.handler import set_ev_cls
 from ryu.ofproto import ofproto_v1_3
 from ryu.lib.mac import haddr_to_bin
